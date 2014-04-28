2a620b93f4cf317b423d24e0f271fc48d3dbbf18
Rodrigo Lima <rodrigoxone@gmail.com>
2014-04-28 20:56:46 -0300

    sales server

--- sales-server/index.php
+++ sales-server/index.php
@@ -1,2 +1,25 @@
 <?php
+session_start();
+require 'app/config/config.php';
+require 'app/DB.php';
+
 require 'vendor/autoload.php';
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim(array(
+    'debug' => false
+));
+
+$app->contentType("application/json");
+
+$app->error(function (\Exception $e = null) use ($app) {
+    echo '{"error":{"text":"'. $e->getMessage() .'"}}';
+});
+
+function formataJson($obj)
+{
+    echo json_encode($obj);
+}
+
+include("customers.php");
+$app->run();
