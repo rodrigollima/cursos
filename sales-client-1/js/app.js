6c0415e036b39d68e00a896e768ae75508d1a4ad
Rodrigo Lima <rodrigoxone@gmail.com>
2014-05-08 22:47:56 -0300

    app.js

---
+++ sales-client-1/js/app.js
@@ -0,0 +1,61 @@
+SERVER_URL = "http://127.0.0.1/";
+
+$app = angular.module('app', []);
+
+$app.config( function ($routeProvider, $httpProvider) {
+   $routeProvider.
+       when('/', {templateUrl: 'view/main.html'}).
+       when('/clientes', {templateUrl: 'view/clientes/main.html', controller:clientesController}).
+       when('/clientes/new', {templateUrl: 'view/clientes/update.html', controller:clientesController}).
+       when('/cliente/:id', {templateUrl: 'view/clientes/update.html', controller:clientesController}).
+       otherwise({redirectTo:'/'});
+
+   $httpProvider.responseInterceptors.push( function ($q, $rootScope) {
+       return function(promise) {
+           $rootScope.hideLoader();
+           return promise.then( function (response) {
+               return response;
+           }, function( response ) {
+               $data = response.data;
+               $error = $data.error;
+               console.error($data);
+               if ($error && $error.text)
+                alert("ERROR: " $error.text);
+               else {
+                   if (response.status=404)
+                       alert("Erro ao acessar oservidor");
+                   else {
+                       alert("ERROR! see log console");
+                   }
+               }
+               return $q.reject(response);
+           });
+       }
+   });
+});
+
+$app.run( function( $rootScope ) {
+
+    $rootScope.showLoaderFlag = false;
+
+    $root.showLoader = function() {
+        $rootScope.showLoaderFlag = true;
+    }
+
+    $rootScope.hideLoader = function() {
+        $rootScope.showLoaderFlag = false;
+    }
+
+    $rootScope.server = function(url) {
+        return SERVER_URL + url;
+    }
+} );
+
+$app.filter( 'startFrom', function () {
+    return function (input, start) {
+        if (input == null)
+            return null;
+        start =+ start;
+        return input.slice(start);
+    }
+});
