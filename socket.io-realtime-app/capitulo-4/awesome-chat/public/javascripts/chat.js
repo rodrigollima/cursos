578d55ccbdb9f71ecf1af6c83efa73a5bc5296f9
Rodrigo Lima <rodrigoxone@gmail.com>
2014-05-19 15:21:16 -0300

    Namespaces

--- socket.io-realtime-app/capitulo-4/awesome-chat/public/javascripts/chat.js
+++ socket.io-realtime-app/capitulo-4/awesome-chat/public/javascripts/chat.js
@@ -1,3 +1,42 @@
+var chatInfra = io.connect('/chat_infra'),
+    chatCom   = io.connect('/chat_com');
+
+chatInfra.on("name_set", function (data) {
+    chatInfra.on("user_entered", function (user) {
+        $('#messages').append('<div class="systemMessage">' + user.name + ' has joined the rom.</div>');
+    });
+
+    chatInfra.on('message', function (message) {
+        var message = JSON.parse(message);
+	$('#messages').append('<div class="'+ message.type + '">' + message.message + '</div>');
+    });
+
+    chatCom.on('message', function (message) {
+        var message = JSON.parse(message);
+	$('#messages').append('<div class="' + message.type + '"><span class="name">' + message.username + ': </span>' +message.message + '</div>');
+    });
+
+    $('#nameForm').hide();
+    $('.popup').hide();
+    $('#messages').append('<div class="systemMessage">Hello ' + data.name + '</div>');
+
+    $('#send').click(function() {
+        var data = {
+	    message:$('#message').val(),
+	    type:'userMessage'
+	};
+        chatCom.send(JSON.stringify(data));
+	$('#message').val();
+    });
+});
+
+$(function () {
+    $('#setname').click(function () {
+        chatInfra.emit("set_name", {name:$('#nickname').val()});
+    });
+});
+
+/*
 var socket = io.connect('/');
 
 socket.on('message', function (data) {
@@ -36,3 +75,4 @@ $(function() {
         socket.emit('set_name', {name:$('#nickname').val()});
     });
 });
+*/
--- socket.io-realtime-app/capitulo-4/awesome-chat/routes/socket.js
+++ socket.io-realtime-app/capitulo-4/awesome-chat/routes/socket.js
@@ -1,6 +1,56 @@
 var io = require('socket.io');
 
 exports.initialize = function (server) {
+
+    io = io.listen(server);
+
+    var chatInfra = io.of("/chat_infra").on("connection", function(socket) {
+        socket.on("set_name", function (data) {
+	    socket.set("nickname", data.name, function () {
+	        socket.emit("name_set", data);
+		socket.send(JSON.stringify({
+		    type:'serverMessage',
+		    message:'Welcome to the chat.' 
+		}));
+		socket.broadcast.emit('user_entered', data);
+	    });
+	});
+    });
+
+    var chatCom = io.of("/chat_com").on("connection", function(socket) {
+        socket.on("message", function(message) {
+	    message = JSON.parse(message);
+	    if (message.type == "userMessage") {
+	        socket.get('nickname', function(err, nickname) {
+		    message.username = nickname;
+		    socket.broadcast.send(JSON.stringify(message));
+		    message.type = "myMessage";
+		    socket.send(JSON.stringify(message));
+		});
+	    }
+	});
+    });    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var io = require('socket.io');
+
+exports.initialize = function (server) {
     io = io.listen(server);
     io.sockets.on("connection", function (socket) {
         //socket.send(JSON.stringify({type:'serverMessage', message:'Welcome to the most interesting chat room'}));
@@ -26,3 +76,4 @@ exports.initialize = function (server) {
 	
     });
 };
+*/
